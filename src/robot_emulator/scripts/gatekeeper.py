9222ca6e38e680e3d2a42106104d4089c26a0ee9
Eric Danz <ericdanziger@gmail.com>
2014-06-24 23:38:37 -0400

    classes

--- src/robot_emulator/scripts/gatekeeper.py
+++ src/robot_emulator/scripts/gatekeeper.py
@@ -4,43 +4,40 @@ from robot_emulator.msg import *
 import rospy
 import sys
 
-def buildModel(data):
-	pass
+class Gatekeeper:
 
-def updateModel(data):
-	pass
+	def __init__(self):
+		rospy.Subscriber('boot', BootResponse, self.buildModel)
+		rospy.Subscriber('outputs', Output, self.updateModel)
+		rospy.Subscriber('errors', Error, self.checkError)
 
-def checkError(data):
-	pass
+		reqPub = rospy.Publisher('reqs', Request, queue_size=10, latch=True)
+		thisRequest = Request()
+		thisRequest.request = 'boot'
+		rospy.loginfo("Sending Boot Request")
+		reqPub.publish(thisRequest)
 
-def init():
-	#init with a boot request from all listening modules
-	rospy.init_node('gatekeeper')
-	reqPub = rospy.Publisher('reqs', Request, queue_size=10, latch=True)
-	thisRequest = Request()
-	thisRequest.request = 'boot'
-	r = rospy.Rate(10)
-	#while not rospy.is_shutdown():
-	#	rospy.loginfo(thisRequest)
-	#	reqPub.publish(thisRequest)
-	#	r.sleep()
-	rospy.loginfo(thisRequest)
-	reqPub.publish(thisRequest)
+	'''def keep(self):
+		reqPub = rospy.Publisher('reqs', Request, queue_size=10, latch=True)
+		thisRequest = Request()
+		thisRequest.request = 'boot'
+		rospy.loginfo("Sending Boot Request")
+		reqPub.publish(thisRequest)	
+	'''
+	def buildModel(self,data):
+		pass
 
-def theSubscriber():
-	#suscribe to all the topics individually
-	#then call something to respond to any activity
-	
-	rospy.loginfo('this is Subscriber')
-	#listen to the three topics coming from the modules
-	rospy.Subscriber('boot', BootResponse, buildModel)
-	rospy.Subscriber('outputs', Output, updateModel)
-	rospy.Subscriber('errors', Error, checkError)
-	rospy.spin()
+	def updateModel(self,data):
+		pass
 
+	def checkError(self,data):
+		pass
 
 
 if __name__ == '__main__':
-	init()
-	theSubscriber()
+	rospy.init_node('gatekeeper')
+	gatekeeper = Gatekeeper()
+	gatekeeper.keep()
+	rospy.loginfo("Gatekeeper Node Started")
+	rospy.spin()
 	
